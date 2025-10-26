4559fb5a8ff7e85f23796aba90972d226c641e30
Utsarg Saxena <jaysaxena2@gmail.com>
2025-10-26 15:48:26 +0530

    Revert "reverted Dummycommit2 dev1" via gitLens
    This reverts commit 17253d75c832fb6e491365bd592049870a18ba50.

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
+                    ---This text is added via Dev1---
         </p>
       </div>
       <p className="read-the-docs">
@@ -34,5 +35,5 @@ function App() {
     </>
   )
 }
-
+ 
 export default App
