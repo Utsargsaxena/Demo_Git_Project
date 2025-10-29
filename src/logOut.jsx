ed08ddd9f6e3758ae80fdc6f1f50251358ecc6c8
Utsarg Saxena <jaysaxena2@gmail.com>
2025-10-29 23:09:36 +0530

    Making continous commits1

--- src/logOut.jsx
+++ src/logOut.jsx
@@ -5,8 +5,9 @@ const logOut = () => {
   <>
     <p> Ignoring the changes doing my unstashed work here more....</p>
   <p>this is header added by utsarg as commt1.</p>
-    <div>logOut making commit1</div>
-  <p>this is a footer added by utsarg as commit2.</p>
+    <div>logOut making commit2 being unaware</div>
+    <div>made an extra change</div>
+  <p>this is a footer added by utsarg as commit2, unaware of this adding some more lines for conflict.</p>
   <p> Ignoring the changes doing my unstashed work here.</p>
   </>
   )
