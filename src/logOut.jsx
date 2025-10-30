9353fd4f9317e3610ec2c019d01ddf7d5fdbc5f6
Utsarg Saxena <jaysaxena2@gmail.com>
2025-10-30 16:43:46 +0530

    resolved changes.

--- src/logOut.jsx
+++ src/logOut.jsx
@@ -7,8 +7,9 @@ const logOut = () => {
     <p>Making some unrelated changes, just to check stashed work</p>
     <p> Ignoring the changes doing my unstashed work here more....</p>
   <p>this is header added by utsarg as commt1.</p>
-    <div>logOut making commit2 being unaware</div>
-    <div>made an extra change</div>
+    <div>logOut making commit2, completely modified this , has to be conflict1</div>
+    <div>made an extra change, added some text adter this... as conflict 2</div>
+    <p> Only to create a conflict via login branch, added a new line below extra change, as conflict 3, if </p>
   <p>this is a footer added by utsarg as commit2, unaware of this adding some more lines for conflict.</p>
   <p> Ignoring the changes doing my unstashed work here.</p>
   <p>More commits1</p>
---
+++ src/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <>
+    <div>login</div>
+    <p>Totally uunrelated work</p>
+    </>
+  )
+}
+
+export default login
